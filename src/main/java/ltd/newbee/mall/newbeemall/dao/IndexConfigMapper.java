fea5980c02f280ddc9c58c27d98e934ba5aef4a2
mi2shopping <wenchangsheng3866@gmail.com>
2022-04-20 12:07:59 +0800

    first commit

--- src/main/java/ltd/newbee/mall/newbeemall/dao/IndexConfigMapper.java
+++ src/main/java/ltd/newbee/mall/newbeemall/dao/IndexConfigMapper.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 import ltd.newbee.mall.newbeemall.entity.IndexConfig;
+import ltd.newbee.mall.newbeemall.entity.NewBeeMallGoods;
 
 public interface IndexConfigMapper {
 
 	List<IndexConfig> findIndexConfigsByTypeAndNum(@Param("configType") int configType, @Param("number") int number);
-	List<NewBeeMallGoods>selectByPrimarkeys(List<Long>goodsIds);
+	
+	List<NewBeeMallGoods> selectByPrimaryKeys(List<Long> goodsIds);
 }
--- src/main/java/ltd/newbee/mall/newbeemall/service/impl/NewBeeMallIndexConfigServiceImpl.java
+++ src/main/java/ltd/newbee/mall/newbeemall/service/impl/NewBeeMallIndexConfigServiceImpl.java
@@ -1,5 +1,6 @@
 package ltd.newbee.mall.newbeemall.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -8,24 +9,33 @@ import org.springframework.stereotype.Service;
 
 import ltd.newbee.mall.newbeemall.dao.IndexConfigMapper;
 import ltd.newbee.mall.newbeemall.entity.IndexConfig;
+import ltd.newbee.mall.newbeemall.entity.NewBeeMallGoods;
 import ltd.newbee.mall.newbeemall.service.NewBeeMallIndexConfigService;
 
 @Service
 public class NewBeeMallIndexConfigServiceImpl implements NewBeeMallIndexConfigService {
 	
+
 	@Resource
     private IndexConfigMapper indexConfigMapper;
 	
 	@Override
-	public List<IndexConfig> getConfigGoodsesForIndex(int configType,int number) {
+	public List<NewBeeMallGoods> getConfigGoodsesForIndex(int configType,int number) {
 		
-		 List<IndexCongfig> idxConfList=indexConfigMapper.findIndexConfigsByTypeAndNum(configType, number);
-		List<Long> ids=new ArrayList<Long>();
-		for(IndexConfig e:idxConfList) {
-			ids.add(e,getGoodsId());
+		List<IndexConfig> idxConfList = indexConfigMapper.findIndexConfigsByTypeAndNum(configType, number);
+		
+		List<Long> ids = new ArrayList<Long>();
+		// List<IndexConfig> => List<Long>
+		for(IndexConfig iConf : idxConfList) {
+			ids.add(iConf.getGoodsId());
+		}
+		List<NewBeeMallGoods>entityList=indexConfigMapper.selectByPrimaryKeys(ids);
+		for(NewBeeMallGoods x:entityList) {
 			
 		}
-		return indexConfigMapper.selectByPrimarkeys(ids);
+		
+		return indexConfigMapper.selectByPrimaryKeys(ids);
 	}
 
 }
+
